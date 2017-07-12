3d8130870b8ccfb93bfc2336761048534781514d
Rocco Bowling <rocco@chimerasw.com>
2017-07-12 16:50:29 -0400

    renaming to UIAlertController_BetterConstructor

---
+++ UIAlertController_BetterConstructor/UIAlertController_BetterConstructor/UIAlertController_BetterConstructor.h
@@ -0,0 +1,19 @@
+//
+//  UIAlertController_BetterConstructor.h
+//  UIAlertController_BetterConstructor
+//
+//  Created by Rocco Bowling on 7/12/17.
+//  Copyright © 2017 Rocco Bowling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIAlertController_BetterConstructor.
+FOUNDATION_EXPORT double UIAlertController_BetterConstructorVersionNumber;
+
+//! Project version string for UIAlertController_BetterConstructor.
+FOUNDATION_EXPORT const unsigned char UIAlertController_BetterConstructorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIAlertController_BetterConstructor/PublicHeader.h>
+
+
